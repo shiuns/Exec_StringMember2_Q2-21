305b9dae798468d34d005785c9c25b4c2615f924
ispan <ispan@DESKTOP-PAKSETB>
2022-10-19 16:04:21 +0800

    加入專案檔案。

---
+++ (Exec_StringMember_Q2)-21 謝孟勳/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Exec_StringMember_Q2__21_謝孟勳
+{
+    //將字串 "Allen Kuo" 轉成大寫 "ALLEN KUO"
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = "Allen Kuo";
+            
+            Console.WriteLine(value.ToUpper());
+        }
+    }
+}
